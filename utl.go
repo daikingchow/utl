40f1400d0681d0edba5be9f18c8826201c31241a
daijinsong <122578138@qq.com>
2015-09-10 16:33:29 +0800

    .

--- utl.go
+++ utl.go
@@ -1,5 +1,18 @@
 package utl
 
+import "unicode"
+
+// 是否闰年
 func isLeap(year int) bool {
 	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
+}
+
+//判断字符串是否包含中文字符
+func IsChineseChar(str string) bool {
+	for _, r := range str {
+		if unicode.Is(unicode.Scripts["Han"], r) {
+			return true
+		}
+	}
+	return false
 }
